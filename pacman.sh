63d4a34b55a0be6bfc14f5af911f3443d1cda33f
Obszczymucha <obszczymucha@gmail.com>
2024-06-19 17:49:46 +1000

    Add missing pacman packages.

--- pacman.sh
+++ pacman.sh
@@ -4,9 +4,57 @@ function main() {
   pacman -Syyu --noconfirm \
     base \
     base-devel \
+    ansible \
+    aria2 \
+    at \
+    binutils \
+    cmus \
+    diffutils \
+    dos2unix \
+    dosfstools \
+    encfs \
+    expac \
+    fastfetch \
+    fd \
+    ffmpeg \
+    fzf \
+    git \
+    git-lfs \
+    gocryptfs \
+    icoutils \
+    inotify-tools \
+    jq \
+    less \
+    lsof \
+    man \
+    mc \
+    mkvtoolnix-gui \
+    ncdu \
+    neovim \
     openssh \
-
-    less
+    pacman-contrib \
+    pkg-config \
+    python \
+    python-pip \
+    python-pipx \
+    rclone \
+    reflector \
+    ripgrep \
+    rsync \
+    rust-src \
+    rustup \
+    stow \
+    svn \
+    the_silver_searcher \
+    tig \
+    tree \
+    unzip \
+    wget \
+    xdg-utils \
+    yt-dlp \
+    zip \
+    zoxide \
+    zsh
 }
 
-main
+main 
